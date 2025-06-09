3d75630a4e7892d5bc172d9cf3b36bee13193bdc
kelanna <hkelan92123@gmail.com>
2025-06-09 15:10:31 -0700

    added login page and card components ui

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,16 +1,16 @@
-import{ cn } from "@/lib/utils"
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
-  CardDescription, 
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-function Login({
+export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
@@ -18,7 +18,10 @@ function Login({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle>Login to your Spotify Account</CardTitle>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <form>
@@ -45,10 +48,19 @@ function Login({
                 <Input id="password" type="password" required />
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full bg-[#1DB954] hover:bg-[#1ED760]">
+                <Button type="submit" className="w-full">
                   Login
                 </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button>
               </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
             </div>
           </form>
         </CardContent>
@@ -56,4 +68,3 @@ function Login({
     </div>
   )
 }
-    export default Login
